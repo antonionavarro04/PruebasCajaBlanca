ef73b79ede9dcc3df148f7ea921c128744e89821
antonionavarro04 <antonio.navarro@iesnervion.es>
2023-03-20 13:13:56 +0100

    Practicar Hoy

--- src/ejercicios/CuentaImpares.java
+++ src/ejercicios/CuentaImpares.java
@@ -4,7 +4,7 @@ public class CuentaImpares {
 
 	public int cuentaImpares(int num) {
 		int cuenta = 0;
-		for (int i = 1; i < num; i++) {
+		for (int i = 1; i <= num; i++) {
 			if (i % 2 == 1) {
 				cuenta++;
 			}
--- src/ejercicios/Tests.java
+++ src/ejercicios/Tests.java
@@ -2,27 +2,42 @@ package ejercicios;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class Tests {
-	@ Test
-	public void camino1() {
+	@ ParameterizedTest
+    @ MethodSource("suma")
+    public void pruebasSuma(int num1, int num2) {
         SumaNumeros suma = new SumaNumeros();
-        int resultado = suma.sumaNumeros(0);
-        assertEquals(0, resultado);
-	}
+        int resultado = suma.sumaNumeros(num1);
+        assertEquals(num2, resultado);
+    }
 
-    @ Test
-    public void camino2() {
-        SumaNumeros suma = new SumaNumeros();
-        int resultado = suma.sumaNumeros(1);
-        assertEquals(0, resultado);
-	}
+    private static Stream<Arguments> suma() {
+        return Stream.of(
+            Arguments.of(0, 0),
+            Arguments.of(1, 0),
+            Arguments.of(2, 2)
+        );
+    }
 
-    @ Test
-    public void camino3() {
-        SumaNumeros suma = new SumaNumeros();
-        int resultado = suma.sumaNumeros(2);
-        assertEquals(2, resultado);
-	}
+    @ ParameterizedTest
+    @ MethodSource("cuenta")
+    public void pruebasImpares(int num1, int num2) {
+        CuentaImpares cuenta = new CuentaImpares();
+        int resultado = cuenta.cuentaImpares(num1);
+        assertEquals(num2, resultado);
+    }
+
+    private static Stream<Arguments> cuenta() {
+        return Stream.of(
+            Arguments.of(0, 0),
+            Arguments.of(1, 1),
+            Arguments.of(2, 1)
+        );
+    }
 }
